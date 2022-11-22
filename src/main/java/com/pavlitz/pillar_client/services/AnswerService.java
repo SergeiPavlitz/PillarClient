eec77c081c74239229d2b826e22887917e96e993
MrWhite <sergeymalek23@gmail.com>
2022-11-22 15:33:53 +0500

    small fixes

--- src/main/java/com/pavlitz/pillar_client/services/AnswerService.java
+++ src/main/java/com/pavlitz/pillar_client/services/AnswerService.java
@@ -1,21 +1,14 @@
 package com.pavlitz.pillar_client.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class AnswerService implements RestService {
-
-    private final Logger log = LoggerFactory.getLogger(AnswerService.class);
 
     private final String URL = "http://localhost:8070/answer";
 
@@ -38,14 +31,11 @@ public class AnswerService implements RestService {
             return false;
         }
 
-        //get all
-//            Phrase[] arr = restTemplate.getForObject(URL + "/listData", Phrase[].class);
-//            log.info(Arrays.toString(arr));
     }
 
     @Override
     public Phrase[] getWeeklyByType(String type) {
-        Phrase[] list = new Phrase[0];
+        Phrase[] list;
         try {
             list = restTemplate.getForObject(URL + "/?pillarTypeWeekly=" + type, Phrase[].class);
             return (list == null) ? new Phrase[0] : list;
